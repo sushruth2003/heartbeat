b92ce3ea07bec9b50bb0ca255c14d1a44804c7d7
Sushruth Booma <sushruth2003@gmail.com>
2022-02-23 17:01:30 -0600

    fixed individual file not reading error

--- batching.go
+++ batching.go
@@ -122,7 +122,7 @@ func main() {
 		log.Fatal(err)
 	}
 	for _, file := range files {
-		readCSV(file.Name())
+		readCSV(absPath + file.Name())
 	}
 	absPath, _ = filepath.Abs("../data/depth")
 	files, err = ioutil.ReadDir(absPath)
